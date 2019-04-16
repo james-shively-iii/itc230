7884c859e97ba0f12c88da2c82a2151161af969b
James Shively III <james.shively-iii@seattlecentral.edu>
2019-04-15 23:49:09 -0700

    Adding the updated server.js file

--- server.js
+++ server.js
@@ -1,30 +1,26 @@
 const http = require("http");
-  http.createServer((req,res) => {
-    console.log("Create server");
-    const path = req.url.toLowerCase();
-      switch(path) {
-        case '/':
-        const fs = require("fs");
-        fs.readFile("public/home.html", (err, data) =>{
-          if (err) return console.error(err);
-          res.writeHead(200, {'Content-Type': 'text/html'});
-          res.end(data.toString());
-        });
-          break;
-        case '/about':
-        //const fs = require("fs");
-        fs.readFile("public/about.html", (err, data) =>{
-          if (err) return console.error(err);
-          res.writeHead(200, {'Content-Type': 'text/html'});
-          res.end(data.toString());
-        });
-          break;
-        default:
-          res.writeHead(404, {'Content-Type': 'text/plain'});
-          res.end('Not found');
-          break;
-        }
-    //res.writeHead(200, {'Content-Type': 'text/plain'});
-    //res.end('Aloha world');
-  }).listen(process.env.PORT || 3000);
-    console.log("Server created");
+const fs = require("fs");
+http.createServer((req,res) => {
+  const path = req.url.toLowerCase();
+  switch(path) {
+    case '/':
+      fs.readFile("public/home.html", (err, data) => {
+        if (err) return console.error(err);
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(data.toString());
+      });
+      break;
+    case '/about':
+      fs.readFile("public/about.html", (err, data) => {
+        if (err) return console.error(err);
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(data.toString());
+      });
+      break;
+    default:
+      res.writeHead(404, {'Content-Type': 'text/plain'});
+      res.end('You must have took a wrong turn');
+      break;
+    }
+}).listen(process.env.PORT || 3000);
+console.log("Server created");
