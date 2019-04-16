589a1f36909832c1ba6dee8b09acf3475dc5ade3
james-shively-iii <james.shively-iii@seattlecentral.edu>
2019-04-16 10:49:43 -0700

    Committing some changes

--- server.js
+++ server.js
@@ -1,19 +1,41 @@
-const http = require("http");  
+const http = require("http");
+const fs = require("fs");
 http.createServer((req,res) => {
+  console.log("Create server");    
   const path = req.url.toLowerCase();
-  switch(path) {
-    case '/':
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end('Home page');
-      break;
-    case '/about':
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end('About page');
-      break;
-    default:
-      res.writeHead(404, {'Content-Type': 'text/plain'});
-      res.end('Not found');
-      break;
+  console.log(path)
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Aloha world');
 }).listen(process.env.PORT || 3000);
 
-console.log("Listen to port 3000");
+/*
+  http.createServer((req,res) => {
+    console.log("Create server");
+    const path = req.url.toLowerCase();
+      switch(path) {
+        case '/':
+
+        fs.readFile("public/home.html", (err, data) =>{
+          if (err) return console.error(err);
+          res.writeHead(200, {'Content-Type': 'text/html'});
+          res.end(data.toString());
+        });
+          break;
+
+        case '/about':
+        //const fs = require("fs");
+        fs.readFile("public/about.html", (err, data) =>{
+          if (err) return console.error(err);
+          res.writeHead(200, {'Content-Type': 'text/html'});
+          res.end(data.toString());
+        });
+          break;
+
+        default:
+          res.writeHead(404, {'Content-Type': 'text/plain'});
+          res.end('Not found');
+          break;
+        }
+  }).listen(process.env.PORT || 3000);
+  */
+console.log("Server created");
