261138b4a647289741690ce8b81a1e3a4bccd423
James Shively III <james.shively-iii@seattlecentral.edu>
2019-05-03 13:52:23 -0700

    Adding all the changes for assignment 3 - 5/3/19

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ import path from 'path';
 
 'use strict'
 //      CONSTANTS AND VARIABLES         \\
-const helm = require('helmet');
-const ex = require('express');
-const bp = require('body-parser');
-const app = ex();
-const P = 3000;
-const albums = require('./albums');
-let hb = require('express-handlebars');
+const helm = require('helmet');                 // helmet for security
+const ex = require('express');                  // express frameworks
+const bp = require('body-parser');              // body parser for express
+const app = ex();                               // placing express into app variable
+const P = 3000;                                 // P is for PORT
+const albums = require('./albums');             // invoking the album array and methods
+const hb = require('express-handlebars');       // express templates
+
 //      INVOKE HANDLEBAR TEMPLATES      \\
 app.engine('.html', hb({ extname: '.html' }));
 app.set('view engine', '.html');
@@ -28,18 +29,25 @@ app.use(bp.urlencoded({ extended:true }));
 
 //      SENDING STATIC FILES            \\
 app.get('/', (req,res) => {
+    // respond in plain text
     res.type('text/html')
+    // respond with a static file
     res.sendFile(__dirname + '/public/home.html')
 });
 app.get('/about', (req,res) => {
+    // respond in plain text
     res.type('text/html')
-    res.sendFile(__dirname + '/public/about.html')
+    // the response back to the user 
+    res.send('About page')
 });
 
 //      404 ERROR HANDLER               \\
 app.use((req,res) => {
+    // respond in plain text
     res.type('text/plain')
+    // response data
     res.status(404)
+    // send the response back to the user
     res.send('404 ERROR: You took the wrong left turn...now meet your demise!!!')
 });
 
@@ -47,27 +55,36 @@ app.use((req,res) => {
 // app.route('/get')
 //     .get((req,res) => {
 //         console.log(req.query)
-//         res.send('here lies my hopes')
+//         res.send('here lies my hopes and dreams')
 //     })
 //     .post((req,res) => 
 //         console.log(req.body)
 // );
 // app.get('/get', (req,res) => {
 //     console.log(req.query)
-//     res.send('here lies my hopes')
+//     res.send('here lies my hopes and dreams')
 // });
 
 //      RENDER METHOD                   \\
-// send content of 'home' view
-app.get('/', (req,res) => {
-    res.render('home')
+// render item deletion 
+app.get('/delete', function(req,res){
+    // look for the item and grab it
+    let result = albums.delete(query.albumTitle); 
+    // take that item and delete it
+    res.render('delete', {title: req.query.albumTitle, result: result});
 });
-app.get('/get', (req,res) => {
-    let result = albums.get(req.query.albumTitle)
-    res.render('details', { albumTitle: req.query.albumTitle, result: result})
+// render item posting
+app.post('/detail', (req,res) => {
+    console.log(req.body)
+    // rendering query into HTML format
+    let header = 'Let\'s look for: ' + req.body.albumTitle + '<br>'
+    // look for the item and grab it
+    let found = albums.get(req.body.albumTitle)
+    // posting that item to the rendered format
+    res.render('details', { albumTitle: req.body.albumTitle, result: found})
 });
 
 
 app.listen(P, () => {
-    console.log(`Server created at port ${P}`)
+    console.log(`Server started with Express and created on port ${P}`)
 });
